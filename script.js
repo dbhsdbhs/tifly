a36353bd9fc378f6c960dfb1d9b82b7d202fdc1f
dbhsdbhs <d0548464360@gmail.com>
2025-05-20 09:08:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let current = 0;
+const testimonials = document.querySelectorAll('.testimonial');
+setInterval(() => {
+  testimonials[current].classList.remove('active');
+  current = (current + 1) % testimonials.length;
+  testimonials[current].classList.add('active');
+}, 4000);
+
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  const formData = new FormData(this);
+  fetch('https://script.google.com/macros/s/YOUhttps://script.google.com/macros/s/AKfycbwh7SpTr7Pd0T669cC9BNhbMFLTIJp67kNjVlpp_4fjKifUCVU9no3b8WOVMbKDQsj9/execR_SCRIPT_ID/exec', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => alert('ההודעה נשלחה בהצלחה!'))
+  .catch(error => alert('אירעה שגיאה בשליחה'));
+  this.reset();
+});
